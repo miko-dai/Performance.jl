dce3a88e18f67ce661878b936a37cd85298e7632
Miko Dai <116597075+miko-dai@users.noreply.github.com>
2023-04-30 21:30:47 -0500

    Update arrays.jl

--- src/arrays.jl
+++ src/arrays.jl
@@ -1,15 +1,19 @@
 export sequence_of_three, alternate_items
 
 function sequence_of_three(start)
-    out = []
+    out = Float64[]
     push!(out, start, start+1, start+2)
     return out
 end
 
 function alternate_items(nrepeats::Integer, item1, item2)
-    out = []
-    for _ = 1:nrepeats           # _ is often used for a variable you'll not use again
-        push!(out, item1, item2)
+    if typeof(item1) != Missing && typeof(item2) != Missing
+        out = zeros(Int64, nrepeats*2)
+    else
+        out = zeros(Union{Missing, Int64}, nrepeats*2)
     end
+    
+    out[1:2:nrepeats*2] .= item1
+    out[2:2:nrepeats*2] .= item2
     return out
 end
